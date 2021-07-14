417e113737965b78732a7d4ec4b29492ecd94313
Ralph Arvin De Castro <ralpharvin.dc@gmail.com>
2021-07-14 13:14:06 -0400

    deleted strapi

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 8082),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "dbedbee0c6792d8f4825298d13dcf022"),
-    },
-  },
-});
--- strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '04521599-3f89-4266-9ba3-40f2bb7ee65b'
-};
